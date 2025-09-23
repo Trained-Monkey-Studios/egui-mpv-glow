1d9a86986d10cadf81dd67c7fadc370f7d67d7b6
Terrence Cole <terrence.d.cole@gmail.com>
2025-09-23 08:52:39 -0700

    Does it work on arm?

--- build.rs
+++ build.rs
@@ -1,3 +1,9 @@
+#[cfg(target_arch = "x86_64")]
 fn main() {
     println!(r"cargo:rustc-link-search=vendor/mpv-dev-x86_64");
+}
+
+#[cfg(target_arch = "aarch64")]
+fn main() {
+    println!(r"cargo:rustc-link-search=vendor/mpv-dev-aarch64");
 }
